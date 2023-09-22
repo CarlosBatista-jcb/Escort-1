bf9a1d7207da51015b4ead3885dcada062237fa5
testegithob aula Santander coded <cbsites10@gmail.com>
2023-09-22 11:36:53 -0300

    Create Index.php

---
+++ Escort/Views/Home/Index.php
@@ -0,0 +1,165 @@
+﻿<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
+    <meta charset="UTF-8" />
+
+<link rel="stylesheet" href="../Escort-1/Escort/Content/camera.css" />
+
+<div id="fullpage">
+    <div id="page1" style="position:relative">
+        <video autoplay="autoplay" loop="loop" data-autoplay data-keepplaying autobuffer="autobuffer" muted="muted" poster="/imgs/escort_logo.png" width="640" height="360">
+            <source data-src="../Escort-1/Escort/videos/ocean1.mp4" src="../Escort-1/Escort/videos/ocean1.mp4" type="video/mp4" />
+
+
+        </video><br><br>
+        <div style="color:white;" class="header-content">
+            <h1 style="font-size: 90px;font-weight:bold">VIP house</h1>
+            <h3 style="font-size: 46px;">Your best moment in Singapore</h3>
+        </div>
+    </div>
+
+    <div id="page2" class="full-page section dark-bg">
+        <div style="padding-left:0;padding-right:0;" class="container-fluid">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 no-padding">
+                <div id="block1" class="block hover-light">
+                    <div style="position:absolute;top:0px;left:20px;">
+                        <h3 class="larger">Party</h3>
+                        <div class="mota">Best things happen in crazy nights</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 no-padding">
+                <div id="block3" class="block  hover-light">
+                    <div style="position:absolute;top:0px;left:20px;">
+                        <h3 class="larger">Angels</h3>
+                        <div class="mota">Live your dream with the hottest & naughtiest girls in Singapore</div>
+                    </div>
+                </div>
+                <div id="block4" class="block hover-light">
+                    <div style="position:absolute;top:0px;left:20px;">
+                        <h3 class="larger">Casino</h3>
+                        <div class="mota">Be on Forbes tonight</div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 no-padding mb-hide">
+                <div id="block2" class="block hover-light">
+                    <div style="position:absolute;top:22px;left:20px;">
+                        <h3 class="larger">Transportation</h3>
+                        <div class="mota">Sorry, Limousine is our only vehicle</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 no-padding mb-hide">
+                <div id="block5" class="block  hover-light">
+                    <div style="position:absolute;top:22px;left:20px;">
+                        <h3 class="larger">Accommodation</h3>
+                        <div class="mota">You can sleep in any corner of Singapore</div>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+
+    <div id="page2-5" class="section dark-bg mb-only">
+        <div style="padding-left:0;padding-right:0;" class="container-fluid">
+
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 no-padding">
+                <div id="block22" class="block hover-light">
+                    <h3 class="larger">Transportation</h3>
+                    <div class="mota">Sorry, Limousine is our only vehicle</div>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 no-padding">
+                <div id="block55" class="block  hover-light">
+                    <h3 class="larger">Accommodation</h3>
+                    <div class="mota">You can sleep in any corner of Singapore</div>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+
+
+    <div id="page3" class="full-page section">
+        <div class="container-fluid" style="background-color:black">
+            <h3 style="text-align:center;" class="larger">Tour Highlights</h3>
+            <div class="container">
+                <div class="row height75">
+                    <div class="camera_wrap camera_azure_skin" id="camera_wrap_1" style="height:100%">
+
+                        <div data-src="/imgs/xexexepng.png">
+
+                        </div>
+                        <div data-src="/imgs/xexexepng.png">
+
+                        </div>
+                        <div data-src="/imgs/pink-aston-martin-and-bikini-babe-for-the-chinese-james-bond_6.jpg">
+                        </div>
+
+
+                    </div><!-- #camera_wrap_1 -->
+                </div>
+            </div>
+
+            <div class="row" style="height:300px">
+                <div style="background-color:black;text-align:center;border-top:2px solid #fff;height:100%" class="col-lg-6 col-md-6 col-sm-6 col-xs-6 no-padding">
+                    <p style="text-align: right;color: white;font-size:22px;margin-top:5px; margin-right:5px;">YOU</p>
+                    <h3 class="h3" style="color:white;height:30px">Let us design your VIP experience</h3>
+                    <div>
+                        <img class="control-w" style="max-height:160px;width: 160px;" src="/imgs/vip_1.png">
+                    </div>
+                </div>
+                <div style="background-color:white;text-align:center;border:2px solid #fff;height:100%" class="col-lg-6 col-md-6 col-sm-6 col-xs-6 no-padding">
+                    <p style="color: black;text-align: left;font-size: 22px;margin-top:5px; margin-left:5px">CAN</p>
+                    <h3 class="h3" style="color:#262626;height:30px">Design your own night</h3>
+                    <div>
+                        <img class="control-w" style="max-height:160px;width: 160px;" src="/imgs/vip_2.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    
+    <div id="page4" class="full-page section dark-bg" style="position:relative">
+        <div id="contact" style="text-align:center;">
+            <h2>Call us now +65 8231 2312</h2>
+            <h2>Or leave your message</h2>
+            <div>
+                <textarea style="font-size: 16px;
+						color: black;
+						width: 354px;" rows="5"></textarea>
+            </div>
+            <button class="btn btn-primary" style="margin-top:10px; font-size:18px">Submit</button>
+        </div>
+
+
+    </div>
+
+</div>
+
+
+<script src="~/Scripts/jquery.fullpage.extensions.min.js"></script>
+<script src="~/lib/js/jquery.cycle.all.js" type="text/javascript" charset="utf-8"></script>
+<script src="~/lib/js/jquery.maximage.js" type="text/javascript" charset="utf-8"></script>
+<script src="~/Scripts/camera.js"></script>
+<script src="~/Scripts/jquery.easing.1.3.js"></script>
+<script src="~/Scripts/main.js"></script>
+<link rel="stylesheet" href="/lib/css/jquery.maximage.css?v=1.2" type="text/css" media="screen" charset="utf-8" />
+
+
+
